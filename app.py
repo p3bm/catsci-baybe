ba8b6ee7f446c8af202e91d5fac4d951e1ae4620
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2025-10-23 14:09:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -236,11 +236,12 @@ def main():
     st.subheader("Continuous Numerical Variables")
 
     num_cont_numerical_variables = st.number_input("How many **continuous numerical** variables do you have?", min_value=0, value=0, key = 'num_cont')
-    cont_numerical_variables_dict = create_continuous_numerical_fields(num_cont_numerical_variables)
 
     if (num_disc_numerical_variables > 0) and (num_cont_numerical_variables > 0):
-        st.error("This tool does support mixing discrete and continuous variables - please use one type or the other exclusively.")
+        st.error("This tool does not support mixing discrete and continuous numerical variables - please use one type or the other exclusively.")
         st.stop()
+    
+    cont_numerical_variables_dict = create_continuous_numerical_fields(num_cont_numerical_variables)
 
     st.subheader("Objectives")
     
