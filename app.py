887cd0ae6dd999804b5adb36d26fb8db844b1854
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2025-11-12 14:39:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -134,8 +134,8 @@ def plot_learning_curve(campaign,objective_dict):
     if num_rounds > 1:
         fig, ax = plt.subplots(figsize=(8, 5))
         for obj in objective_dict:
-            st.write(objective_dict)
             values = objective_dict[obj]
+            st.write(values)
             if values["mode"].lower() == 'max':
                 y = info.groupby('BatchNr')[obj].max().reset_index()
             else:
