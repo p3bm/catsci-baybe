681b8200dc6d868d06e64393191ffb0056eb7e02
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2025-11-14 14:21:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -111,6 +111,7 @@ def recommend_input():
     past_recommendation = st.toggle('Include existing reaction data')
     if past_recommendation:
         df = upload_file(key='Reactions data CSV')
+        st.write(df.shape)
         return df
     return None
 
