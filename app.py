2a7d3423370a5fd639f16e49a1bc26701d1b85ad
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2025-11-14 10:01:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -380,11 +380,11 @@ def main():
 
     campaign_previous = upload_file(key='latest campaign JSON')
     
-    batch_reactions = st.number_input("Number of reactions to suggest", min_value= 1, value= 1, key = 'batch')
+    batch_size = st.number_input("Number of reactions to suggest", min_value= 1, value= 1, key = 'batch')
     df = recommend_input()
     
     if st.button("Get recommendations"):
-        st.session_state.reactions, st.session_state.new_campaign = recommend_reactions(campaign_previous, df, batch_reactions)
+        st.session_state.reactions, st.session_state.new_campaign = recommend_reactions(campaign_previous, df, batch_size)
         if st.session_state.reactions is not None and st.session_state.new_campaign is not None:
             st.data_editor(st.session_state.reactions)
             st.session_state.recommendations_made = True
